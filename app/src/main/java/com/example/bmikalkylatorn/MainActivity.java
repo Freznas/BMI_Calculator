499e4352f78214534cef0845607b75e11362b782
Jocke_B <joakim198904@live.se>
2023-10-12 16:39:26 +0200

    Updated Design and changed to english text.

--- app/src/main/java/com/example/bmikalkylatorn/MainActivity.java
+++ app/src/main/java/com/example/bmikalkylatorn/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    //Här deklarerar vi variabler med passande simpla namn.
     TextView textView;
     Button helloButton;
     EditText inputheight;
@@ -22,15 +23,20 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//här nedan hämtar vi IDn för olika delar av appen.
         textView = findViewById((R.id.tv_instructions));
         inputheight = findViewById(R.id.et_height);
         inputweight = findViewById(R.id.et_weight);
         textView.setText(R.string.instruction);
-
         helloButton = findViewById(R.id.btn_calculate);
         helloButton.setOnClickListener(new View.OnClickListener()
+
         {
+            /**
+             *
+             * @param v här skapar vi en onclick metod som skickar upp en Toast när man klickar på
+             *          knappen som även ändrar texten för instruktioner till resultat av uträkningen av BMI.
+             */
             @Override
             public void onClick(View v)
             {
@@ -38,42 +44,44 @@ public class MainActivity extends AppCompatActivity
                 calculateBmi();
 
 
+
             }
         });
     }
 
-    public void getheight()
-    {
-        String message = inputheight.getText().toString();
-        textView.setText(message);
-    }
-
-    public void getweight()
-    {
-        String message = inputweight.getText().toString();
-        textView.setText(message);
-    }
-
+    /**
+     * Här skapar vi strängar som hämtar inputen från längd och vikt inmatning.
+     */
     public void calculateBmi()
     {
         String heightStr = inputheight.getText().toString();
         String weightStr = inputweight.getText().toString();
+        /**
+         * här skapar vi en if sats som tar inmatade strängar och kollar så det finns inmatning
+         * i bägge så gör dem en uträkning av BMI på de båda talen
+         */
         if (!heightStr.isEmpty() && !weightStr.isEmpty())
         {
+            //här görs inmatningarna om från sträng till int.
             int height = Integer.parseInt(heightStr);
             int weight = Integer.parseInt(weightStr);
 
             double bmi = calculateBmi(height, weight);
+            //Här begränsar vi antalet decimaler i uträkningen för hålla det enkelt och rent.
             String formattedBmi = String.format("%.2f", bmi);
             textView.setText(("Ditt BMI är:" + formattedBmi));
+
         }
+        //om någon av villkoren inte uppfylls(dvs. att om antigen längd eller vikt fälten är tomma)
+        //så får användaren en Toast som uppmanar att följa instruktionerna.
         else
         {
             Toast.makeText(MainActivity.this,"fyll i både längd och vikt,",Toast.LENGTH_LONG).show();
+
         }
 
     }
-
+//Här sker uträkningen av BMIn baserat på inmatningen.
     private double calculateBmi(int height, int weight)
     {
         // Beräkna BMI enligt formeln (vikt / (längd * längd))
